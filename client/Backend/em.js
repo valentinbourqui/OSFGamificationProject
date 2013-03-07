22879c6ff5850b49ffcc2bb08df750b85b3df751
Valentin Bourqui <valentin.bourqui@gmail.com>
2013-03-08 00:47:56 +0100

    resquest post and get

--- client/Backend/em.js
+++ client/Backend/em.js
@@ -1,22 +1,58 @@
 
 var council = "No id";
 
-$.ajax({
-		dataType: "json",
-		url: "http://localhost:3000/test",
-		async : false,
-		type: 'get',			
-		success : function(data) {
-			   council = data;
-	    }
-});	
+var callAPI = function (params) {
+    var url = (typeof(params) === 'string' ? params : params.url),
+        data = params.data || {},
+        type = params.type || params.method || 'GET',
+        call;
+  if (type === 'GET') {
+    myData = $.param({});
+	call = $.getJSON(url, myData);
+  }
+  else if (type=='POST') {
+   $.post(url, data, function(data, textStatus) {
+	 alert(data+"POST request OK ");
+}, "json");
+  }
+  call
+  .success(function (res) {
+    $('#content').append(JSON.stringify(res) + '<br/>');
+  })
+  .error(function (err) {
+    console.log(err);
+  });
+};
 
+// Call API with GET
+callAPI('http://127.0.0.10:3000/test');
+
+// Call API with PUT
+callAPI({
+  url: 'http://127.0.0.10:3000/login',
+  method: 'POST',
+  data: {
+    user: 'valentin',
+    pass: 'testpass'
+  }
+});
+/*$.ajax({
+      url: 'http://127.0.0.10:3000/test',
+       type: 'get',
+      contentType: 'application/json',
+      dataType: 'json',
+      success: function (data) {
+          alert(data);
+          }
+     
+    });*/
+    
 
 Songs = Ember.Application.create({
     mixmaster: council,
     totalReviews: 0,
     ready: function(){
-		alert(council);
+		//alert(council);
     }
 });
 
@@ -27,6 +63,8 @@ Songs.Song = Ember.Object.extend({
     genre: null,
     listens: 0
 });
+
+
 
 Songs.songsController = Ember.ArrayController.create({
     content: [],
